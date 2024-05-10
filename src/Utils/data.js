e0539ebdb231bc2a7e6d526458cd9a8b05d17bc2
Apichat Wongtala <apichat26870@gmail.com>
2024-05-10 14:11:25 +0700

    two commit

---
+++ src/Utils/data.js
@@ -0,0 +1,89 @@
+export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "https://cdn-icons-png.freepik.com/512/8099/8099466.png",
+    skills: [
+      { skill: "HTML5", percentage: "80%" },
+      { skill: "CSS3", percentage: "75%" },
+      { skill: "JavaScript", percentage: "65%" },
+      { skill: "React.js", percentage: "60%" },
+      { skill: "TailwindCSS", percentage: "50%" },
+    ],
+  },
+  {
+    title: "Backend",
+    icon: "https://cdn-icons-png.flaticon.com/512/12238/12238274.png",
+    skills: [
+      { skill: "MySQL", percentage: "50%" },
+      { skill: "Node.js", percentage: "50%" },
+      { skill: "Express.js", percentage: "50%" },
+    ],
+  },
+  {
+    title: "Tools",
+    icon: "https://cdn-icons-png.flaticon.com/512/7181/7181976.png",
+    skills: [
+      { skill: "Visual Studio Code", percentage: "75%" },
+      { skill: "Git & GitHub", percentage: "70%" },
+      { skill: "fsdf", percentage: "76%" },
+      { skill: "khjk", percentage: "78%" },
+    ],
+  },
+  {
+    title: "Soft Skills",
+    icon: "https://cdn-icons-png.freepik.com/512/8301/8301097.png",
+    skills: [
+      { skill: "Viskhjkh", percentage: "75%" },
+      { skill: "tykhj", percentage: "74%" },
+      { skill: "liulu", percentage: "72%" },
+      { skill: "ewdff", percentage: "79%" },
+    ]
+  }
+];
+
+export const WORK_EXPERIENCE = [
+  {
+    title: "Administrative officer litigation work at Yukwiwat Law Company Limited",
+    date: "October 2021 - November 2023",
+    responsibilities: [
+    "Oversaw emails for job notifications from Kiatnakin Phatra Bank (Public Company Limited) and coordinated with internal bank officers responsible for our company.",
+    "Handled data sent by the bank, reported it to the company's main report, and utilized the gathered information in subsequent steps of our company's workflow, such as:",
+    "Prepared initial data for debt collection processes.",
+    "Sent payment demand letters based on information received from the contracting bank.",
+    "Entered data for daily reports, where our company's lawyers filed cases through the court's filing system or E-Filing system and engaged in legal proceedings in various formats.",
+    "Reported through Line groups for admin to manage company data, along with email and pcol.kkpfg.com system reports to the contracting bank.",
+    "Facilitated checking the number of received and completed jobs for performance evaluation against competitors and ranking job acceptance for the following month.",
+    "Coordinated with employees within and outside the company and performed tasks assigned by the supervisor.",
+  ],
+  },
+  {
+    title: "Workshop Portfolio Website Responsive ",
+    date: "January 2024 - February 2024",
+    responsibilities: [
+      "A responsive portfolio website is one that is designed to look good on any device, whether it be desktop, tablet or mobile. Creating a responsive portfolio website allows you to showcase your work. and your abilities to employers or clients effectively",
+      "On the portfolio website There are details of the work. Includes details about the project, images, and links to existing projects.",
+      "There is a contact form so users can get in touch. This may include other contact channels such as email or social media.",
+    ],
+  },
+  {
+    title: "Workshop Website Todolist",
+    date: "January 2024 - February 2024",
+    responsibilities: [
+      "A to-do list helps you keep track of all the tasks you need to complete. By listing them out, you can prioritize what needs to be done first and allocate your time accordingly",
+      "Writing down tasks ensures that you won’t forget important things. It’s easy to overlook tasks when they’re only in your mind, but a to-do list acts as a reliable reminder",
+      "With a to-do list, you can allocate time for each task and avoid wasting time on less important activities. It helps streamline your workflow and keeps you focused on what matters most",
+      "Tools like Todoist allow teams to organize work together. You can create shared workspaces where team members collaborate on tasks while keeping their personal to-do lists separate. This feature is especially useful for project management and team coordination",
+
+    ],
+  },
+  {
+    title: "Workshop project-react-shopping-app-api",
+    date: "January 2024 - February 2024",
+    responsibilities: [
+    "The Shopping Cart React Application website is part of creating an online application for shopping.",
+    "Add to Cart: Adding products to cart is the process in which users select the products they want to purchase and add them to their cart. This usually involves clicking the “Add to Cart” or similar button on the product page.",
+    "Remove from Cart: Removing products from cart is a process in which the user selects the products that they want to remove from the cart. This usually involves clicking the “Remove” or similar button on the cart page.",
+    "Calculate Total Order: When the user adds or deletes items in the cart. The system must calculate the total amount of all products in the cart. This includes the price of each product. and show the total to the user."
+  ],
+  },
+]
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import './Skills.css'
+import React, { useState } from "react";
+import "./Skills.css";
+import { SKILLS } from "../../Utils/data";
+import SkillCard from "./SkillCard/SkillCard";
+import SkillSInfoCard from "./SkillSInfoCard/SkillSInfoCard";
+
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
 
-export default Skills
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data);
+  };
+
+  return (
+    <section className="skills-container">
+      <h5>Technical Proficiency</h5>
+
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCard
+              key={item.title}
+              iconUrl={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectSkill(item);
+              }}
+            />
+          ))}
+        </div>
+
+        <div className="skills-info">
+          <SkillSInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
